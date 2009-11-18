f6fb0024255c85934bf91e282ae2eed13d83d2a8
j.horatiu.vasile <j.horatiu.vasile@faad8746-7ae3-11de-b278-e305e7926026>
2009-11-18 18:25:07 +0000

    increased sleep

--- DimmunixLDPreloadEfficient/test/HawkNLStandalone/src/deadlock3.c
+++ DimmunixLDPreloadEfficient/test/HawkNLStandalone/src/deadlock3.c
@@ -16,14 +16,14 @@ NLenum socktype = NL_RELIABLE_PACKETS;
 void* closeThread(void* socket)
 {
   int i = 0;
-  
+
   //while (i < 0x1234567)
   //i++;
 
   usleep(10000);
 
   NLsocket s = *(NLsocket *) socket;
-  
+
   nlClose(s);
   printf("nl close finished\n");
   return (void*)i;
@@ -31,12 +31,12 @@ void* closeThread(void* socket)
 
 void* shutdownThread(void *s)
 {
-  
+
   int i = 0;
   //while (i < 0x1223000)
   // i++;
 
-  usleep(10000);
+  usleep(500000);
 
   nlShutdown();
   printf("nl shutdown finished\n");
@@ -47,7 +47,7 @@ void* shutdownThread(void *s)
 void printErrorExit(void)
 {
   NLenum err = nlGetError();
-  
+
   if(err == NL_SYSTEM_ERROR)
     {
       printf("System error: %s\n", nlGetSystemErrorStr(nlGetSystemError()));
@@ -67,45 +67,44 @@ int main(int argc, char **argv)
   NLthreadID tid1, tid2;
   NLenum type = NL_IP;
   NLint exitcode;
-  
+
 
   int i = 0;
 
   for(i = 0; i< 1; i++)
     {
-      
+
       if(!nlInit())
 	printErrorExit();
-      
+
 
       if(!nlSelectNetwork(type))
 	printErrorExit();
-      
-      
+
+
       if (argc > 1)
 	serverport = atoi(argv[1]);
-      
-      
+
+
       serversock = nlOpen(serverport, socktype); /* just a random port number ;) */
-      
-      
+
+
       if(serversock == NL_INVALID)
 	{
 	  printf("serversock == NL_INVALID %ld %d\n", (long) serversock, NL_INVALID);
 	  printErrorExit();
 	}
-      
+
       printf("creating threads\n");
 
       tid1 = nlThreadCreate(closeThread, (void *) & serversock, NL_TRUE);
       tid2 = nlThreadCreate(shutdownThread, (void*) & serversock, NL_TRUE);
-      
+
       nlThreadJoin(tid1, (void ** ) &exitcode);
       nlThreadJoin(tid2, (void ** ) &exitcode);
-      sleep(2);
-    }    
-  
-  
+    }
+
+
   return 0;
 }
 
