0676c9196bceaa5d263607d4c7224a67307df957
j.horatiu.vasile <j.horatiu.vasile@faad8746-7ae3-11de-b278-e305e7926026>
2009-11-18 13:59:23 +0000

    hawknl test

---
+++ trunk/DimmunixLDPreloadEfficient/test/HawkNLStandalone/samples/deadlock2.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "nl.h"
+#include <unistd.h>
+
+
+NLushort serverport = 25002;
+NLenum socktype = NL_RELIABLE_PACKETS;
+
+
+void* closeThread(void* socket)
+{
+  int i = 0;
+  
+  while (i < 0x1234567)
+    i++;
+
+  usleep(10000);
+
+  NLsocket s = *(NLsocket *) socket;
+  nlClose(s);
+  return (void*)i;
+}
+
+void* shutdownThread(void *s)
+{
+  
+  int i = 0;
+  
+  while (i < 0x1223000)
+    i++;
+
+  usleep(10000);
+
+  nlShutdown();
+  return (void*) i;
+}
+
+
+void printErrorExit(void)
+{
+  NLenum err = nlGetError();
+  
+  if(err == NL_SYSTEM_ERROR)
+    {
+      printf("System error: %s\n", nlGetSystemErrorStr(nlGetSystemError()));
+    }
+  else
+    {
+      printf("HawkNL error: %s\n", nlGetErrorStr(err));
+    }
+  nlShutdown();
+  exit(1);
+}
+
+
+int main(int argc, char **argv)
+{
+  NLsocket serversock;
+  NLthreadID tid1, tid2;
+  NLenum type = NL_IP;
+  NLint exitcode;
+  
+
+  int i = 0;
+
+  for(i = 0; i< 1000; i++)
+    {
+      
+      if(!nlInit())
+	printErrorExit();
+      
+      if(!nlSelectNetwork(type))
+	printErrorExit();
+      
+      
+      if (argc > 1)
+	serverport = atoi(argv[1]);
+      
+      
+      serversock = nlOpen(serverport, socktype); /* just a random port number ;) */
+      
+      if(serversock == NL_INVALID)
+	printErrorExit();
+      
+      tid1 = nlThreadCreate(closeThread, (void *) & serversock, NL_TRUE);
+      tid2 = nlThreadCreate(shutdownThread, (void*) & serversock, NL_TRUE);
+      
+      nlThreadJoin(tid1, (void ** ) &exitcode);
+      nlThreadJoin(tid2, (void ** ) &exitcode);
+    }    
+  
+  
+  return 0;
+}
+
