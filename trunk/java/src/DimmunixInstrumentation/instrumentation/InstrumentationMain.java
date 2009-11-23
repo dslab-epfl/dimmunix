e8925169c5ed941e7af0ac8358f2bec36935241b
j.horatiu.vasile <j.horatiu.vasile@faad8746-7ae3-11de-b278-e305e7926026>
2009-11-23 17:17:05 +0000

    checkpoint

--- trunk/java/src/DimmunixInstrumentation/instrumentation/InstrumentationMain.java
+++ trunk/java/src/DimmunixInstrumentation/instrumentation/InstrumentationMain.java
@@ -1,3 +1,29 @@
+/*
+     Created by Horatiu Jula, George Candea, Daniel Tralamazza, Cristian Zamfir
+     Copyright (C) 2009 EPFL (Ecole Polytechnique Federale de Lausanne)
+
+     This file is part of Dimmunix.
+
+     Dimmunix is free software: you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the
+     Free Software Foundation, either version 3 of the License, or (at
+     your option) any later version.
+
+     Dimmunix is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public
+     License along with Dimmunix. If not, see http://www.gnu.org/licenses/.
+
+     EPFL
+     Dependable Systems Lab (DSLAB)
+     Room 330, Station 14
+     1015 Lausanne
+     Switzerland
+*/
+
 package instrumentation;
 
 import java.lang.instrument.Instrumentation;
